c9e4515598b395ae788a9998859544a54693f4e6
sswmouse <1422536287@qq.com>
2021-06-08 15:34:30 +0800

    s

---
+++ demo/pages/homePage/homePage.js
@@ -0,0 +1,72 @@
+// pages/homePage/homePage.js
+const APP = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 轮播图
+    imgs: [APP.globalUrl.url + "images/index_banner.png", APP.globalUrl.url + "images/banner.png"],
+    // 选项图标，文字
+    icon: [APP.globalUrl.url + "images/ruku.png", APP.globalUrl.url + "images/chuku.png", APP.globalUrl.url + "images/ks.png", APP.globalUrl.url + "images/spfl.png", APP.globalUrl.url + "images/member.png"],
+    text: ['货品入库', '货品出库', '库存管理', '商品分类', '人员管理'],
+    url: ['/pages/ruku_gl/ruku_gl', "/pages/chuku_gl/chuku_gl","/pages/kucun_gl/kucun_gl","/pages/good_gl/good_gl","/pages/people_gl/people_gl"]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // console.log(APP.globalUrl.url)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ demo/pages/mine/mine.js
@@ -0,0 +1,96 @@
+// pages/mine/mine.js
+const APP = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
+    head_img: APP.globalUrl.url + "images/" + "default_avatar.png",  //头像图片
+    help_icon: APP.globalUrl.url + "images/" + "help_icon.png",  //帮助中心图标
+    gywm_icon: APP.globalUrl.url + "images/" + "about_us_icon.png",  //关于我们图标
+    set_icon: APP.globalUrl.url + "images/" + "set_icon.png",  //设置图标
+    arrow_icon: APP.globalUrl.url + "images/" + "gray_arrow.png",  //箭头图标
+  },
+  exit() {
+    this.setData({ 
+      hasUserInfo: false 
+    })
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        console.log(res.userInfo)
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
